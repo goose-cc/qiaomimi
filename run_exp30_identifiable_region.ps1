1bbcf0756307d35d32376ff14269431036b319ae
goose-cc <24301018@bjtu.edu.cn>
2026-08-21 08:25:34 +0800

    98%

--- run_exp30_identifiable_region.ps1
+++ run_exp30_identifiable_region.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$Device = "cuda",
-    [string]$AliasThresholds = "0,0.25,0.5,0.75",
+    [string]$AliasThresholds = "0.2,0.25,0.3,0.35,0.4,0.5",
     [double]$MinSeparationRMSSNR = 1.3,
     [double]$MaxCoverRMSSNR = 2.0,
     [switch]$Quick,
